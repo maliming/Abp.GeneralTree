4dc5cb5112c772f7414dd6f28fecd8404009444c
maliming <malimings@gmail.com>
2017-05-17 14:10:50 +0800

    update abp 2.0.2

--- Module-GeneralTree/Properties/AssemblyInfo.cs
+++ Module-GeneralTree/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Abp.GeneralTree")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Abp.GeneralTree")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("maliming")]
 [assembly: AssemblyProduct("Abp.GeneralTree")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]

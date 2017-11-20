fcc0f4f66159b6bf953c83e53446adb5ce40bcdf
maliming <malimings@gmail.com>
2017-11-20 18:02:38 +0800

    TreeApplication

---
+++ TreeApplication/TreeAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TreeApplication
+{
+    public class TreeAppDbContext : AbpDbContext
+    {
+        public TreeAppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Region> Region { get; set; }
+
+        public virtual DbSet<Region2> Region2 { get; set; }
+    }
+}
---
+++ TreeApplication/TreeAppModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Abp.EntityFrameworkCore;
+using Abp.Modules;
+using Abp.GeneralTree;
+using Castle.MicroKernel.Registration;
+using Microsoft.EntityFrameworkCore;
+
+namespace TreeApplication
+{
+    [DependsOn(typeof(AbpEntityFrameworkCoreModule), typeof(GeneralTreeModule))]
+    public class TreeAppModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            IocManager.IocContainer.Register(
+                Component
+                    .For<DbContextOptions<TreeAppDbContext>>()
+                    .Instance(new DbContextOptionsBuilder<TreeAppDbContext>()
+                        .UseInMemoryDatabase("MockDB")
+                        .Options)
+                    .LifestyleSingleton()
+            );
+
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}

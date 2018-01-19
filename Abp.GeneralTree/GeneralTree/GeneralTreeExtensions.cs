e296f929eca73061ac6e3a00d2f43bb525b6397f
maliming <malimings@gmail.com>
2018-01-19 13:31:11 +0800

    GeneralTreeExtensions ToTree

---
+++ Abp.GeneralTree/GeneralTree/GeneralTreeExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abp.GeneralTree
+{
+    public static class GeneralTreeExtensions
+    {
+        public static IEnumerable<TTree> ToTree<TTree, TPrimaryKey>(this IEnumerable<TTree> tree)
+            where TPrimaryKey : struct
+            where TTree : class, IGeneralTree<TTree, TPrimaryKey>
+        {
+            var treeDic = tree.ToDictionary(x => x.Id);
+
+            treeDic.Where(x => x.Value.ParentId.HasValue).Select(x => x.Value).ForEach(x =>
+            {
+                // ReSharper disable once PossibleInvalidOperationException
+                var parent = treeDic[x.ParentId.Value];
+                if (parent.Children == null)
+                {
+                    parent.Children = new List<TTree>();
+                }
+                parent.Children.Add(x);
+            });
+
+            return treeDic.Values.Where(x => x.ParentId == null);
+        }
+
+        public static IEnumerable<TTree> ToTreeWithReferenceType<TTree, TPrimaryKey>(this IEnumerable<TTree> tree)
+            where TPrimaryKey : class
+            where TTree : class, IGeneralTreeWithReferenceType<TTree, TPrimaryKey>
+        {
+            var treeDic = tree.ToDictionary(x => x.Id);
+
+            treeDic.Where(x => x.Value.ParentId != null).Select(x => x.Value).ForEach(x =>
+            {
+                // ReSharper disable once PossibleInvalidOperationException
+                var parent = treeDic[x.ParentId];
+                if (parent.Children == null) {
+                    parent.Children = new List<TTree>();
+                }
+                parent.Children.Add(x);
+            });
+
+            return treeDic.Values.Where(x => x.ParentId == null);
+        }
+    }
+}
---
+++ TreeTests/GeneralTreeExtensions_Tests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.GeneralTree;
+using Shouldly;
+using Xunit;
+
+namespace TreeTests
+{
+    internal class Regin : Entity<long>, IGeneralTree<Regin, long>
+    {
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Code { get; set; }
+
+        public int Level { get; set; }
+
+        public Regin Parent { get; set; }
+
+        public long? ParentId { get; set; }
+
+        public ICollection<Regin> Children { get; set; }
+    }
+
+    internal class Regin2 : Entity<string>, IGeneralTreeWithReferenceType<Regin2, string>
+    {
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Code { get; set; }
+
+        public int Level { get; set; }
+
+        public Regin2 Parent { get; set; }
+
+        public string ParentId { get; set; }
+
+        public ICollection<Regin2> Children { get; set; }
+    }
+
+    public class GeneralTreeExtensions_Tests : TreeTestBase
+    {
+        [Fact]
+        public void ToTree_Test()
+        {
+            var regions = new List<Regin>
+            {
+                new Regin
+                {
+                    Id = 1,
+                    Name = "北京",
+                },
+                new Regin
+                {
+                    Id = 2,
+                    Name = "东城区",
+                    ParentId = 1
+                },
+                new Regin
+                {
+                    Id = 3,
+                    Name = "西城区",
+                    ParentId = 1
+                },
+                new Regin
+                {
+                    Id = 4,
+                    Name = "河北",
+                },
+                new Regin
+                {
+                    Id = 5,
+                    Name = "石家庄",
+                    ParentId = 4
+                },
+                new Regin
+                {
+                    Id = 6,
+                    Name = "承德",
+                    ParentId = 4
+                },
+                new Regin
+                {
+                    Id = 7,
+                    Name = "双桥区",
+                    ParentId = 6
+                }
+            };
+
+            var tree = regions.ToTree<Regin, long>().ToList();
+
+            tree.ShouldNotBeNull();
+            tree.Count.ShouldBe(2);
+            tree.First().Children.Count.ShouldBe(2);
+            tree.Last().Children.Count.ShouldBe(2);
+            tree.Last().Children.Last().Children.Count.ShouldBe(1);
+        }
+
+        [Fact]
+        public void ToTree_WithReferenceType_Test()
+        {
+            var regions = new List<Regin2>
+            {
+                new Regin2
+                {
+                    Id = "1",
+                    Name = "北京",
+                },
+                new Regin2
+                {
+                    Id = "2",
+                    Name = "东城区",
+                    ParentId = "1"
+                },
+                new Regin2
+                {
+                    Id = "3",
+                    Name = "西城区",
+                    ParentId = "1"
+                },
+                new Regin2
+                {
+                    Id = "4",
+                    Name = "河北",
+                },
+                new Regin2
+                {
+                    Id = "5",
+                    Name = "石家庄",
+                    ParentId = "4"
+                },
+                new Regin2
+                {
+                    Id = "6",
+                    Name = "承德",
+                    ParentId = "4"
+                },
+                new Regin2
+                {
+                    Id = "7",
+                    Name = "双桥区",
+                    ParentId = "6"
+                }
+            };
+
+            var tree = regions.ToTreeWithReferenceType<Regin2, string>().ToList();
+
+            tree.ShouldNotBeNull();
+            tree.Count.ShouldBe(2);
+            tree.First().Children.Count.ShouldBe(2);
+            tree.Last().Children.Count.ShouldBe(2);
+            tree.Last().Children.Last().Children.Count.ShouldBe(1);
+        }
+    }
+
+}
